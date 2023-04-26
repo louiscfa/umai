2b9d0569c161c7a6c3b1d38d8b94f1a6ce3b3e21
Yassine <80360559+Yassine-Bel@users.noreply.github.com>
2023-04-26 16:46:53 +0200

    Acces
    Ajout de la page d'accès

--- umai_front/src/app/app.module.ts
+++ umai_front/src/app/app.module.ts
@@ -22,7 +22,7 @@ import { AccesComponent } from './acces/acces.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
